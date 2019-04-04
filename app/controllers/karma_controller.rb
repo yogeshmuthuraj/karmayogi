2615ba59a7bd0f4a569141714b8e51a6bb01f79c
Yogesh Muthuraj <Yogesh.Muthuraj@cerner.com>
2019-04-04 17:34:12 +0530

    try init

--- app/controllers/karma_controller.rb
+++ app/controllers/karma_controller.rb
@@ -2,6 +2,6 @@ class KarmaController < ApplicationController
   def add
     p 'in add'
     responseMsg = '{ "type": "message", "text": "Error: message sender cannot be authenticated." }'
-    render json: responseMsg
+    render plain: responseMsg
   end
 end
