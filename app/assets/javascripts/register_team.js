88610aa2f075e935b3d4e12c93cbbc784a569629
Yogesh Muthuraj <Yogesh.Muthuraj@cerner.com>
2019-04-05 21:04:13 +0530

    try

--- app/assets/javascripts/register_team.js
+++ app/assets/javascripts/register_team.js
@@ -1,18 +1,18 @@
-import asyncHandler from './vendor/asyncHandler';
-
-function registerTeam(attributes) {
-  const promiseObject = asyncHandler('/team', 'POST', attributes);
-  promiseObject.then(() => {
-    // Nothing to do here
-  }, () => {
-    // Nothing to do here
-  });
-}
-
-
-
-
-$(document).on('click', '#register-team-btn', function(event) {
-  event.preventDefault();
-  registerTeam();
-});
+// import asyncHandler from './vendor/asyncHandler';
+//
+// function registerTeam(attributes) {
+//   const promiseObject = asyncHandler('/team', 'POST', attributes);
+//   promiseObject.then(() => {
+//     // Nothing to do here
+//   }, () => {
+//     // Nothing to do here
+//   });
+// }
+//
+//
+//
+//
+// $(document).on('click', '#register-team-btn', function(event) {
+//   event.preventDefault();
+//   registerTeam();
+// });
