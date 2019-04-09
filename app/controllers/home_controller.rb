b2aff6a7b6cf78ea8691f30dcd49dfedf317099f
Yogesh Muthuraj <Yogesh.Muthuraj@cerner.com>
2019-04-09 17:13:23 +0530

    m

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,36 +6,67 @@ class HomeController < ApplicationController
   def index
   end
 
+  def leaderboard
+  end
+
   def service
     # now st: wSg5K8wLhk1OFHi1GddsfGJ5XtC1B6+yEi+V82wUVqk=
     team_id = params[:channelData][:teamsTeamId]
 
     if Team.exists?(team_id: team_id)
-      p 'request.env'
-      p request.env['HTTP_AUTHORIZATION']
-
-      key = 'SwahCt15L2rZyKpaBVNOUgubN4veD8A8xCGKylr+NDg='
-      data = request.env.inspect.encode("iso-8859-1").force_encoding("utf-8")
-      digest = OpenSSL::Digest.new('sha256')
-
-      p 'digest gen'
-      p digest
-
-      p 'digest'
-      p OpenSSL::HMAC.digest(digest, key, data)
-      #=> "\xDE|\x9B\x85\xB8\xB7\x8A\xA6\xBC\x8Az6\xF7\n\x90p\x1C\x9D\xB4\xD9"
-      p 'hexp digest'
-      p OpenSSL::HMAC.hexdigest(digest, key, data)
-      #=> "de7c9b85b8b78aa6bc8a7a36f70a90701c9db4d9"
+      p 'in ifTeam.exists?(team_id: team_id)'
+      # TODO check for security token match
+      # var payload = '';
+    	# // Process the request
+    	# request.on('data', function (data) {
+    	# 	payload += data;
+    	# });
+      # const bufSecret = Buffer(sharedSecret, "base64");
+      # var msgBuf = Buffer.from(payload, 'utf8');
+      # "HMAC " + crypto.createHmac('sha256', bufSecret).update(msgBuf).digest("base64");
 
 
-      responseMsg = '{ "type": "message", "text": "Error: Team is not registered." }'
+      # key = 'wSg5K8wLhk1OFHi1GddsfGJ5XtC1B6+yEi+V82wUVqk='
+      # data = request.body.read.encode('UTF-8').bytes
+      # hash  = OpenSSL::Digest::Digest.new('sha256')
+      # calculated_hmac = Base64.encode64(OpenSSL::HMAC.digest(hash, key, data)).strip
+      #
+      # p 'calculated_hmac'
+      #
+      # p "HMAC #{calculated_hmac}"
+      #
+      # responseMsg = '{ "type": "message", "text": "Error: Team is not registered." }'
+      #
+      # render plain: responseMsg
+      # p 'HTTP_AUTHORIZATION'
+      # p request.env['HTTP_AUTHORIZATION']
 
-      render plain: responseMsg
+      # Remove new line and HTML tags
+      received_msg = ActionController::Base.helpers.strip_tags(params[:text]).gsub(/\R+/, '').split(' ')
+
+      p 'received_msg'
+      p received_msg[1]
+
+      # TODO: Change to switch
+      if received_msg[1] == 'leaderboard'
+        p 'params[:text]'
+        p params[:text]
+      else
+        mentioned_name = params[:entities][0][:mentioned][:name]
+        # Whether ++ or --
+        charge = ActionController::Base.helpers.strip_tags(params[:text]).gsub(/\R+/, '').split(mentioned_name)[1]
+
+        p 'charge'
+        p charge
+      end
+
+      response_msg = '{ "type": "message", "text": "Error: Team is not registered." }'
+
+      render plain: response_msg
     else
-      responseMsg = '{ "type": "message", "text": "Error: Team is not registered." }'
+      response_msg = '{ "type": "message", "text": "Error: Team is not registered." }'
 
-      render plain: responseMsg
+      render plain: response_msg
     end
   end
 end
