dbedf35c21cb04912c94baa4656d0c46b289fd14
Yogesh Muthuraj <Yogesh.Muthuraj@cerner.com>
2020-08-21 15:43:47 +0530

    TRigger

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -65,6 +65,9 @@ class HomeController < ApplicationController
       response_msg = %Q({ "type": "message", "text": "Error: The Team is not registered." })
     end
 
+    p 'debugy response_msg'
+    p response_msg
+
     render plain: response_msg
   end
 end
